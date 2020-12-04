8d31463e7057f835c7acf968db95c36249df7e05
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-12-04 14:58:52 +0000

    Deploy jenkasia/MP_Task to github.com/jenkasia/MP_Task.git:gh-pages

---
+++ precache-manifest.6999f5b3e6d31cb2615a2febe1e568e8.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "805b0e1b6f87c6f4bac616005ae3eba4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8a8af315dbbc8bd75e54",
+    "url": "./static/css/main.edd401de.chunk.css"
+  },
+  {
+    "revision": "26eaf8b022a383179450",
+    "url": "./static/js/2.c67491ac.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.c67491ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a8af315dbbc8bd75e54",
+    "url": "./static/js/main.1998677b.chunk.js"
+  },
+  {
+    "revision": "ad59b82d75f9ed875792",
+    "url": "./static/js/runtime-main.6ce93715.js"
+  },
+  {
+    "revision": "255a3bb63118354dfe177ce60e06835e",
+    "url": "./static/media/Roboto-Bold.255a3bb6.ttf"
+  },
+  {
+    "revision": "3d3075e4cfcc38e0e7164e46f91a20b5",
+    "url": "./static/media/Roboto-Bold.3d3075e4.svg"
+  },
+  {
+    "revision": "7fe6d103e99074ba3e2a05aeb52bfd32",
+    "url": "./static/media/Roboto-Bold.7fe6d103.woff"
+  },
+  {
+    "revision": "c830620c676dd8790e2e317fb94fcb0c",
+    "url": "./static/media/Roboto-Bold.c830620c.eot"
+  },
+  {
+    "revision": "3af1eaf1b8cf9d9255fc3c4c0263ea1f",
+    "url": "./static/media/Roboto-Medium.3af1eaf1.eot"
+  },
+  {
+    "revision": "7a730f0276d5cb1f1076a6396efd7b80",
+    "url": "./static/media/Roboto-Medium.7a730f02.woff"
+  },
+  {
+    "revision": "cc2ca12fa8b96c2ea94711a4872a3e95",
+    "url": "./static/media/Roboto-Medium.cc2ca12f.ttf"
+  },
+  {
+    "revision": "d1d5a5a519e6cd8c044d71dd6f5eef59",
+    "url": "./static/media/Roboto-Medium.d1d5a5a5.svg"
+  },
+  {
+    "revision": "70bba8c684766cc8244eedb1f10b8a51",
+    "url": "./static/media/Roboto.70bba8c6.ttf"
+  },
+  {
+    "revision": "87d067c8dfc595eba42ff9d94b382d57",
+    "url": "./static/media/Roboto.87d067c8.eot"
+  },
+  {
+    "revision": "e73a9e3c6f7e3e1681951618a71bcd2c",
+    "url": "./static/media/Roboto.e73a9e3c.woff"
+  },
+  {
+    "revision": "f8ef482557892f269d70bc66c475a9d1",
+    "url": "./static/media/Roboto.f8ef4825.svg"
+  }
+]);
